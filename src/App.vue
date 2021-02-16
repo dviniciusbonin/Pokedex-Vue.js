e3a159b85e6b5e277b3ed5b9c3bc9c907b0833fc
DVinciusBonin <vinibonin007@gmail.com>
2021-02-16 17:08:16 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+    <div class="column is-half is-offset-one-quarter">
+       <img class="img" src="https://img.ibxk.com.br/2019/09/30/30091641838086.jpg?w=1120&h=420&mode=crop&scale=both" alt="logo">
+       <hr>
+       <h2 class="is-size-2 is-flex is-justify-content-center">Pokedex</h2>
+  
+        <input id="input" class="input is-rounded is-info mb-4 ml-2 " type="text" placeholder="Buscar pokemon pelo nome" v-model="busca">
+       
+        <button id="button" class="button mb-2 ml-2 large-button is-success large-button" @click="filtrar">Buscar</button>
+        <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
+          <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+
+      </div>
+
+    </div>
+    
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios'
+import Pokemon from './components/Pokemon'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Pokemon
+  },
+  data() {
+    return {
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
+    }
+  },
+  created:function() {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=152&offset=0').then(
+      resp => {
+       
+        this.pokemons = resp.data.results
+        this.filteredPokemons = resp.data.results
+      }
+    )
+  },
+  methods: {
+    filtrar: function() {
+      this.filteredPokemons = this.pokemons
+      if(this.busca == '' || this.busca == ' ') {
+        this.filteredPokemons = this.pokemons
+      }else {
+        this.filteredPokemons = this.pokemons.filter(
+          pokemon => pokemon.name == this.busca
+        )
+        
+      }
+
+
+    }
+  },
+  computed: {
+    resultadoBusca: function() {
+      if(this.busca == '' || this.busca == ' ') {
+        return this.pokemons
+      }else {
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
   }
 }
+ 
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#input {
+  width: 70%;
+
 }
+
+#button {
+  width: 25%;
+}
+
+.img {
+  border-radius: 2em;
+}
+
 </style>
